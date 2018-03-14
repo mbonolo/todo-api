14009c58df1f2e7f107087e9f18147bd7b452437
Marco Bonolo <bonolomarco@gmail.com>
2018-03-14 14:00:47 +0100

    Add DELETE /todos/:id endpoint

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,22 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove({});
+// Todo.findByIdAndRemove();
+
+// Todo.findOneAndRemove({
+//   _id: '5aa9002839596b32b922044b'
+// }).then((todo) => {
+//   console.log(todo);
+// });
+
+Todo.findByIdAndRemove('5aa9002839596b32b922044b').then((todo) => {
+  console.log(todo);
+});
