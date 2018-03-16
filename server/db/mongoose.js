550d62c6d350b9141869fb6615d453e19b4187cd
Marco Bonolo <bonolomarco@gmail.com>
2018-03-16 09:29:11 +0100

    Hash user password before saving

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;    //Set use of promise in mongoose instead of callbacks
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
